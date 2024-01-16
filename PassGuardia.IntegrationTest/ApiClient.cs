da0514fe2426827b5f786a01f39788222ba07fa7
Chebanu <0alexchebanu@gmail.com>
2024-01-16 17:27:13 +0200

    Integration tests pt1

---
+++ PassGuardia.IntegrationTest/ApiClient.cs
@@ -0,0 +1,107 @@
+﻿using Flurl.Http;
+
+using PassGuardia.Contracts.Http;
+using PassGuardia.Domain.Commands;
+using PassGuardia.Domain.Queries;
+
+namespace PassGuardia.IntegrationTest;
+
+internal interface IApiClient : IDisposable
+{
+    Task<CreatePasswordResult> CreatePassword(PasswordRequest passwordRequest, string token);
+    Task<GetPasswordByIdResult> GetPassword(string passwordId);
+    Task<GetPasswordByIdResult> GetPassword(string passwordId, string token);
+    Task<RegisterUserResponse> RegisterUser(RegisterUserRequest registerUserRequest);
+    Task<AuthenticateUserResponse> AuthenticateUser(AuthenticateUserRequest authenticateUserRequest);
+    Task<MeResponse> Me(string token);
+    Task AdminUpdate(AdminUpdateUserCommand adminUpdateCommand, string token); 
+}
+
+internal class ApiClient : IApiClient
+{
+    private readonly FlurlClient _client;
+
+    public ApiClient(HttpClient client)
+    {
+        _client = new FlurlClient(client);
+    }
+
+    public Task AdminUpdate(AdminUpdateUserCommand request, string token)
+    {
+        return _client
+                .Request()
+                .AppendPathSegment("users")
+                .AppendPathSegment("admin")
+                .WithOAuthBearerToken(token)
+                .AllowHttpStatus(200)
+                .PutJsonAsync(request);
+    }
+
+    public Task<AuthenticateUserResponse> AuthenticateUser(AuthenticateUserRequest authenticateUserRequest)
+    {
+        return _client
+            .Request()
+            .AppendPathSegment("users")
+            .AppendPathSegment("authenticate")
+            .AllowHttpStatus(200)
+            .PostJsonAsync(authenticateUserRequest)
+            .ReceiveJson<AuthenticateUserResponse>();
+    }
+
+    public Task<CreatePasswordResult> CreatePassword(PasswordRequest passwordRequest, string token)
+    {
+        return _client
+            .Request()
+            .AppendPathSegment("passwords")
+            .AllowHttpStatus(201)
+            .PostJsonAsync(passwordRequest)
+            .ReceiveJson<CreatePasswordResult>();
+    }
+
+    public void Dispose()
+    {
+        _client?.Dispose();
+    }
+
+    public Task<GetPasswordByIdResult> GetPassword(string passwordId)
+    {
+        return _client
+            .Request()
+            .AppendPathSegment("passwords")
+            .AppendPathSegment(passwordId)
+            .AllowHttpStatus(200)
+            .GetJsonAsync<GetPasswordByIdResult>();
+    }
+
+    public Task<GetPasswordByIdResult> GetPassword(string passwordId, string token)
+    {
+        return _client
+            .Request()
+            .AppendPathSegment("passwords")
+            .AppendPathSegment(passwordId)
+            .WithOAuthBearerToken(token)
+            .AllowHttpStatus(200)
+            .GetJsonAsync<GetPasswordByIdResult>();
+    }
+
+    public Task<MeResponse> Me(string token)
+    {
+        return _client
+            .Request()
+            .AppendPathSegment("users")
+            .AppendPathSegment("me")
+            .WithOAuthBearerToken(token)
+            .AllowHttpStatus(200)
+            .GetJsonAsync<MeResponse>();
+    }
+
+    public Task<RegisterUserResponse> RegisterUser(RegisterUserRequest registerUserRequest)
+    {
+        return _client
+            .Request()
+            .AppendPathSegment("users")
+            .AllowHttpStatus(200)
+            .PostJsonAsync(registerUserRequest)
+            .ReceiveJson<RegisterUserResponse>();
+    }
+}
---
+++ PassGuardia.IntegrationTest/Base.cs
@@ -0,0 +1,63 @@
+﻿using Bogus;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+using PassGuardia.Contracts.Http;
+
+using Xunit;
+
+namespace PassGuardia.IntegrationTest;
+
+internal class UserInfo
+{
+    public string Id { get; init; }
+    public string Username { get; init; }
+    public string Password { get; init; }
+    public string Role { get; init; }
+    public string Token { get; init; }
+}
+
+public  class Base : IClassFixture<WebApplicationFactory<Program>>, IDisposable
+{
+    protected readonly Faker _faker;
+    private protected readonly IApiClient _apiClient;
+    private readonly IServiceScope _scope;
+
+    public Base(WebApplicationFactory<Program> factory)
+    {
+        _apiClient = new ApiClient(factory.CreateClient());
+        _scope = factory.Services.CreateScope();
+        _faker = new Faker();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _scope?.Dispose();
+            _apiClient?.Dispose();
+        }
+    }
+
+    private protected async Task<UserInfo> CreateTestUser(string role = "user")
+    {
+        var registrationRequest = new RegisterUserRequest
+        {
+            Username = $"t_{_faker.Database.Random.Uuid():N}",
+            Password = $"Ab1!_{_faker.Internet.Password()}"
+        };
+
+        var registrationResponse = await _apiClient.RegisterUser(registrationRequest);
+
+        var roleManager = _scope.ServiceProvider.GetRequiredService<RoleManger<IdentityRole>>();
+    }
+
+}
